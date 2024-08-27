3416a0ebb5763709318daa2060a96eec36e2853a
Giovanni Ortu <giovortu@gmail.com>
2024-08-27 14:01:18 +0200

    Fxed default secrets

--- include/secrets_default.h
+++ include/secrets_default.h
@@ -1,9 +1,9 @@
 #ifndef SECRETS_H
 #define SECRETS_H
 
-inline const char *BROKER ="";
-inline const char *ssid = "";
-inline const char *password = "";
+const char *BROKER ="";
+const char *ssid = "";
+const char *password = "";
 
 
 #endif
